5ddc5c336009895e336ee1f0921c4b859d7c9218
jellybean175 <78124071+jellybean175@users.noreply.github.com>
2024-02-19 09:20:39 -0500

    Update potential_inquiries.sql

--- potential_inquiries.sql
+++ potential_inquiries.sql
@@ -1,4 +1,4 @@
--- Assuming the CSV file has been imported into a table named 'Top 100 Best Nintendo Games of All Time' with a column named 'Genre'
+-- The CSV file has been imported into a table named 'Top 100 Best Nintendo Games of All Time' with a column named 'Genre'
 
 -- Create a temporary table to store the genre counts
 CREATE TEMP TABLE GenreCounts AS
